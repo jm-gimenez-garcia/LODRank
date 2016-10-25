e5ccf0097b551e2e493bebe6f1aa10b8f6077c2f
José M. Giménez-García <jose.gimenez.garcia@univ-st-etienne.fr>
2016-10-25 18:02:16 +0200

    Now trying to get the public suffix when the top private domain fails (relevant when the host part only includes publix suffix, such as amazonaws.com)

--- src/main/java/eu/wdaqua/lodrank/urlprocessor/URLtoPLD.java
+++ src/main/java/eu/wdaqua/lodrank/urlprocessor/URLtoPLD.java
@@ -20,7 +20,10 @@ public class URLtoPLD extends URLProcessor {
 	public String getDataset() {
 		try {
 			return this.url != null ? InternetDomainName.from(this.url.getHost()).topPrivateDomain().toString() : null;
-		} catch (final IllegalArgumentException | IllegalStateException e) {
+		} catch (final IllegalStateException e) {
+			this.logger.debug(this.url.getHost() + " is not under public suffix. Trying to get public suffix intead.");
+			return InternetDomainName.from(this.url.getHost()).publicSuffix() != null ? InternetDomainName.from(this.url.getHost()).publicSuffix().toString() : null;
+		} catch (final IllegalArgumentException e) {
 			this.logger.warn("Could not extract link because " + this.url.toString() + " has not a valid domain name.");
 			return null;
 		}
